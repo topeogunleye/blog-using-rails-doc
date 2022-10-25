95caa06871e3788f6fbabcd5434aa212bc33c45d
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-10-25 20:47:47 +0100

    Fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
-
   # https://guides.rubyonrails.org/getting_started.html#resourceful-routing
-  resources :articles
+
+  resources :articles do
     resources :comments
   end
 end
