442e9148512da4b4853178e1e0825ec473f6284c
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-10-29 13:12:57 +0100

    Add authentication for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
+
+  # snippet for brevity
+
 
   def show
     @article = Article.find(params[:id])
